5c0cbc54071f88dd0319caa37b1f4a291f42dedd
Mikayla Konst <mikayla.konst@gmail.com>
2017-07-09 19:58:50 -0400

    added javascript to generate random fact

---
+++ script.js
@@ -0,0 +1,14 @@
+var fun_facts = ["I love cats", "I play piano", "I am not a morning person"]
+
+// Returns a random fact
+function random_fact(){
+  return fun_facts[rand_int(0,fun_facts.length)];
+}
+// Returns a random integer between min (inclusive) and max (exclusive)
+function rand_int(min, max){
+  return Math.floor(Math.random()*(max-min)) + min;
+}
+
+function change_text(){
+  document.getElementById("fact").innerHTML = random_fact();
+}
